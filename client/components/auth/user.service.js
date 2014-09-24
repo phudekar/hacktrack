1bb65e0570a1f2d2633010cf9b3d2b1d9fc81497
Douglas Vaz <douglas1992@gmail.com>
2014-09-24 12:15:21 +0530

    Basic template

---
+++ client/components/auth/user.service.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('hacktrackApp')
+  .factory('User', function ($resource) {
+    return $resource('/api/users/:id/:controller', {
+      id: '@_id'
+    },
+    {
+      changePassword: {
+        method: 'PUT',
+        params: {
+          controller:'password'
+        }
+      },
+      get: {
+        method: 'GET',
+        params: {
+          id:'me'
+        }
+      }
+	  });
+  });
